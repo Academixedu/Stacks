e192fecb5a30db95079dfbee0c70112473f1796f
chdkumar <chokkakulakumar1123@gmail.com>
2024-07-24 18:52:08 +0530

    Stacks

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,21 @@
+import java.util.Stack;
+
+import java.util.Iterator;
+
 public class Dynamic{
 public static void main(String[]args){
 // Create an Object for Stack
-  // Load person class Objects in it using push function
+Stack<Persons> st = new Stack<Persons>();
+  // Load person class Objects in it using push function;
+  st.push(new Persons("nani", 25, "developer", 25000));
+  st.push(new Persons("sai", 20, "manager", 35000));
+  st.push(new Persons("sri", 30, "web developer", 45000));
+  st.push(new Persons("sri", 40, "tester",50000));
   // Iterate it by using List Iterator
+  Iterator<Persons> i = st.listIterator();
   // print the values by using while Loop
+  while(i.hasNext()){
+    System.out.println(i.next());
+  }
 }
 }
--- Enumerations.java
+++ Enumerations.java
@@ -1,8 +1,18 @@
+import java.util.Stack;
+import java.util.Enumeration;
+
 public class Enumerations{
   public static void main(String[]args){
    // Create an Object for Stack
+   Stack<Persons> st = new Stack<Persons>();
   // Load person class Objects in it
+  st.push(new Persons("nani", 25, "backend developer", 25000));
+  st.push(new Persons("sai", 20, "Data Scientist", 35000));
   // Iterate it by using Enumeration
+  Enumeration<Persons> e = st.elements();
   // print the values by using while Loop
+  while(e.hasMoreElements()){
+    System.out.println(e.nextElement());
+  }
 
   }}
