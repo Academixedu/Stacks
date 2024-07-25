a9b5197273e8069e1f21438ba4988a2e20e45af2
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-07-26 00:15:17 +0530

    done

--- Enumerations.java
+++ Enumerations.java
@@ -1,8 +1,21 @@
+import java.util.Stack;
+import java.util.Enumeration;
+
 public class Enumerations{
   public static void main(String[]args){
    // Create an Object for Stack
   // Load person class Objects in it
   // Iterate it by using Enumeration
-  // print the values by using while Loop
+  // print the values by using while Loop 
+
+    Stack<person> s = new Stack<person>();
+    s.push(new person("Ram", 10));
+    s.push(new person("Shyam", 20));
+
+    Enumeration e = s.elements();
+    while(e.hasMoreElements()){
+      System.out.println(e.nextElement());
+    }
+
 
   }}
---
+++ person.java
@@ -0,0 +1,14 @@
+public class person {
+
+    String name;
+    int age;
+    public person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "person [name=" + name + ", age=" + age + "]";
+    }
+    
+}
