f472f39e9ee5a5bf302c6b3cebc54e8015413943
jahnavi0545 <kandiboyinajahnavi@gmail.com>
2024-07-25 12:35:43 +0530

    stack

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,18 @@
+import java.util.Stack;
+import java.util.Iterator;
+//import javax.swing.text.html.HTMLDocument.Iterator;
 public class Dynamic{
 public static void main(String[]args){
 // Create an Object for Stack
+Stack<Person> s = new Stack<Person>();
   // Load person class Objects in it using push function
+  s.push(new Person(1, "Surya", "Hyderabad"));
+  s.push(new Person(2, "madhuri", "Chennai"));
   // Iterate it by using List Iterator
+  Iterator i = s.listIterator();
   // print the values by using while Loop
+  while(i.hasNext()){
+    System.out.println(i.next());
+  }
 }
 }
