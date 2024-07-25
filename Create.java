656cefc72aa473a11b50b2348a9143746a32fc7a
srinadh0405 <srinadh.nallabothula@email.com>
2024-07-25 13:09:15 +0530

    Stacks Done

--- Create.java
+++ Create.java
@@ -1,7 +1,28 @@
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Create{
 public static void main(String[]args){
 // Create an Object for Stack
+
+    Stack<Integer> s = new Stack<Integer>();
   // Load Values in Stack
+
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  s.push(40);
   // Iterate it by usig Iterator and While Loops
+
+  Iterator iterator = s.listIterator();
+  while(iterator.hasNext()){
+    System.out.println(iterator.next());
+  }
+
+  
+
+  
 }
 }
