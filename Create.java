87905c4a24bcf69aa30d50f1a7d7573597779b1a
KOushikguptha <koushikguptha2003@gmail.com>
2024-07-25 11:44:37 +0530

    Stacks

--- Create.java
+++ Create.java
@@ -1,7 +1,29 @@
-public class Create{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load Values in Stack
-  // Iterate it by usig Iterator and While Loops
-}
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Create {
+    public static void main(String[] args) {
+        // Create an Object for Stack
+        Stack<Integer> stack = new Stack<>();
+
+        // Load Values in Stack
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+
+        // Iterate using Iterator
+        System.out.println("Iterating using Iterator:");
+        Iterator<Integer> iterator = stack.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        // Iterate using while loop
+        System.out.println("Iterating using while loop:");
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
 }
