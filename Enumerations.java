36e75a1f9afb7a3040cee1a662999c2f9db4732e
Naveentogaru1 <togarunaveen1@gmail.com>
2024-07-26 18:50:15 +0530

    Stacks/Create.java

--- Enumerations.java
+++ Enumerations.java
@@ -1,8 +1,19 @@
+
+import java.util.Enumeration;
+import java.util.Stack;
+
 public class Enumerations{
   public static void main(String[]args){
    // Create an Object for Stack
   // Load person class Objects in it
   // Iterate it by using Enumeration
   // print the values by using while Loop
+  Stack<Person> List = new Stack<Person>();
+  List.push(new Person (1,"Togar"));
+  List.push(new Person(2,"Naveen")); 
+  Enumeration<Person> i = List.elements();
+  while(i.hasMoreElements()){
+      System.out.println(i.nextElement().toString());
+  }
 
   }}
