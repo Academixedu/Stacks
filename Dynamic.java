88bea8557a11fa62001a2571a60131c2bf57f6fe
siva-245711 <sivanagendra809@gmail.com>
2024-07-26 12:48:40 +0530

    stack

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,23 @@
-public class Dynamic{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load person class Objects in it using push function
-  // Iterate it by using List Iterator
-  // print the values by using while Loop
-}
+
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Dynamic {
+    public static void main(String[] args) {
+        // Create an Object for Stack
+        // Load Personss class Objects in it using push function
+        // Iterate it by using List Iterator
+        // Print the values by using while Loop
+        Stack<Person> list = new Stack<Person>();
+
+        Person p1 = new Person("siva", 24, "Male", "Delhi");
+        Person p2 = new Person("shiva", 21, "Male", "kkd");
+        list.push(p1);
+        list.push(p2);
+        Iterator<Person> load = list.listIterator();
+        while (load.hasNext()) {
+            System.out.println(load.next());
+        }
+    }
 }
