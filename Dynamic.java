87e2007df0fbcfc3e76862bedf2d6ee60ece7df9
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-12 19:11:38 +0530

    from fayaz

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,36 @@
-public class Dynamic{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load person class Objects in it using push function
-  // Iterate it by using List Iterator
-  // print the values by using while Loop
-}
-}
+import java.util.Stack;
+import java.util.Iterator;
+
+public class Dynamic {
+    public static void main(String[] args) {
+        // Create an object for Stack
+        Stack<Person> s = new Stack<>();
+
+        // Load Person class objects into the Stack using push function
+        s.push(new Person("fayaz", 21));
+        s.push(new Person("sai", 21));
+        s.push(new Person("karthik", 21));
+
+        // Iterate over the Stack using Iterator
+        Iterator<Person> i = s.iterator();
+
+        // Print the values using while loop
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+    }
+  }
+    class Person {
+      String name;
+      int age;
+  
+      Person(String name, int age) {
+          this.name = name;
+          this.age = age;
+      }
+  
+      public String toString() {
+          return name + "  " + age;
+      }
+  }
+
--- Enumerations.java
+++ Enumerations.java
@@ -1,8 +1,38 @@
+import java.util.Stack;
+import java.util.Enumeration;
+
 public class Enumerations{
-  public static void main(String[]args){
    // Create an Object for Stack
   // Load person class Objects in it
   // Iterate it by using Enumeration
   // print the values by using while Loop
 
-  }}
+
+    public static void main(String[] args) {
+    
+        Stack<Person> s = new Stack<>();
+    
+        s.push(new Person("fayaz", 21));
+        s.push(new Person("sai", 21));
+        s.push(new Person("karthik", 21));
+
+        Enumeration<Person> enumeration = s.elements();
+
+        while (enumeration.hasMoreElements()) {
+            System.out.println(enumeration.nextElement());
+        }
+    }
+  }
+    class Person {
+      String name;
+      int age;
+  
+      Person(String name, int age) {
+          this.name = name;
+          this.age = age;
+      }
+  
+      public String toString() {
+          return name + "  " + age;
+      }
+  }
